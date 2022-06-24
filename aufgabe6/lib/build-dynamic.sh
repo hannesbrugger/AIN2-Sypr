f6cdfe86183b5b23a0403e5465c74c37b2caf9b9
marc1107 <m.bohner99@gmail.com>
2022-06-24 10:52:11 +0200

    Aufgabe 6

--- aufgabe6/lib/build-dynamic.sh
+++ aufgabe6/lib/build-dynamic.sh
@@ -3,7 +3,7 @@ for s in benotung.cpp fachnote.cpp fachnoten_liste.cpp ; do
   compile_command="g++ -g -fPIC  -Wall -Wextra -Werror -Weffc++ -Wold-style-cast -std=c++11 -pedantic   -c $s"
     echo $compile_command
     eval $compile_command
-    if [ $? ne 0 ] ; then
+    if [ $? -ne 0 ] ; then
         echo build failed
         exit 1
     fi
@@ -11,7 +11,7 @@ done
 link_command="g++ -shared -o libaufgabe6.so benotung.o fachnote.o fachnoten_liste.o"
 echo $link_command
 eval $link_command
-if [ $? ne 0 ] ; then
+if [ $? -ne 0 ] ; then
     echo build failed
     exit 1
 fi
