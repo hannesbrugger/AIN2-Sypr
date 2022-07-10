4db0c178082048ce07510c7d8b5e37d725a137b9
marc1107 <m.bohner99@gmail.com>
2022-07-10 14:07:30 +0200

    Pruefungsvorbereitung

---
+++ Pruefungsvorbereitung/SYPR-Probeklausur/aufgabe4/aufgabe4.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		return 1;
+	}
+
+	char *s = argv[1];
+	char *t = argv[2];
+	char *st = (char *) malloc(strlen(s) + strlen(t) + 1);
+	strcpy(st, s);
+	strcat(st, t);
+	return 0;	
+}
